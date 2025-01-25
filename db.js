02cd9dcac11af8ecc5a3e2d5effa8a868e48de64
Pedro Thomé <pedro.th.dev@gmail.com>
2025-01-25 14:24:57 -0300

    alterando variaveis ambiente e conexao c producao 2

--- db.js
+++ db.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  host: isProduction ? process.env.DB_PROD_HOST : process.env.DB_LOCAL_HOST,
-  port: isProduction ? process.env.DB_PROD_PORT : process.env.DB_LOCAL_PORT,
-  database: isProduction ? process.env.DB_PROD_NAME : process.env.DB_LOCAL_NAME,
-  user: isProduction ? process.env.DB_PROD_USER : process.env.DB_LOCAL_USER,
-  password: isProduction ? process.env.DB_PROD_PASSWORD : process.env.DB_LOCAL_PASSWORD,
-  ssl: isProduction ? { rejectUnauthorized: false } : false, // Adicione isso para evitar problemas com SSL no Railway
+    connectionString,
+    ssl: isProduction ? { rejectUnauthorized: false } : false, // Adicione isso para evitar problemas com SSL no Railway
 });
 
 pool.connect()
